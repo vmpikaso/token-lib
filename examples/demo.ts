0929c1be0ef673f9a25f2c8cb2a9fc3227e64902
Victor MARFIL <vicmarvu98@gmail.com>
2026-09-19 10:56:34 +0200

    refactor: replace index.ts by export lib and move current code in demo file

---
+++ examples/demo.ts
@@ -0,0 +1,46 @@
+import {
+  SEPARATOR,
+  TOKEN_TYPE,
+  Token,
+  TokenColumn,
+  TokenField,
+  TokenHeader,
+  TokenHeaderWithList,
+  TokenList,
+  TokenText,
+} from "../src/index.js";
+
+const token = new Token({
+  type: TOKEN_TYPE.TEXT,
+  value: "hello",
+  hidden: false,
+});
+const tokenField = new TokenField({
+  value: "pouet",
+  addQuote: false,
+  hidden: false,
+});
+const tokenList = new TokenList({
+  value: "liste",
+  alias: "alias",
+  hidden: false,
+});
+const tokenColumn1 = new TokenColumn({ value: "Entete1", hidden: false });
+const tokenHeader = new TokenHeader({
+  content: ["entete1", "entete2"],
+  hidden: false,
+});
+const tokenHeaderWithList = new TokenHeaderWithList({
+  list: tokenList,
+  columns: [tokenColumn1],
+  hidden: false,
+});
+const tokenText = new TokenText({
+  type: TOKEN_TYPE.SEPARATOR,
+  value: SEPARATOR.SEMICOLON.value,
+  hidden: false,
+});
+const tokens = [tokenHeader, tokenHeaderWithList, token, tokenField, tokenList, tokenText];
+
+console.log(tokenHeaderWithList.getTitle());
+console.log(tokens.reduce((acc, token) => `${acc}${token.toString()}`, ""));
--- src/index.ts
+++ src/index.ts
@@ -1,106 +1,22 @@
-import { Token } from "./class/token.js";
-import { TokenColumn } from "./class/token-column.js";
-import { TokenField } from "./class/token-field.js";
-import { TokenHeader, TokenHeaderWithList } from "./class/token-header.js";
-import { TokenList } from "./class/token-list.js";
-import { TokenText } from "./class/token-text.js";
-import { SEPARATOR } from "./constants/separator.js";
-import { TOKEN_TYPE } from "./constants/token-type.js";
-import type { TokenGlobal } from "./interfaces/token-global.js";
+export { Token } from "./class/token.js";
+export { TokenColumn } from "./class/token-column.js";
+export { TokenCondition } from "./class/token-condition.js";
+export { TokenContainer } from "./class/token-container.js";
+export { TokenExpression } from "./class/token-expression.js";
+export { TokenField } from "./class/token-field.js";
+export { TokenHeader } from "./class/token-header.js";
+export { TokenHeaderWithList } from "./class/token-header-with-list.js";
+export { TokenList } from "./class/token-list.js";
+export { TokenOperator } from "./class/token-operator.js";
+export { TokenText } from "./class/token-text.js";
 
-const token = new Token({
-  type: TOKEN_TYPE.TEXT,
-  value: "hello",
-  hidden: false,
-});
-const tokenField = new TokenField({
-  value: "pouet",
-  addQuote: false,
-  hidden: false,
-});
-const tokenList = new TokenList({
-  value: "liste",
-  alias: "alias",
-  hidden: false,
-});
-const tokenColumn1 = new TokenColumn({ value: "Entete1", hidden: false });
-const tokenHeader = new TokenHeader({
-  content: ["entete1", "entete2"],
-  hidden: false,
-});
-const tokenHeaderWithList = new TokenHeaderWithList({
-  list: tokenList,
-  columns: [tokenColumn1],
-  hidden: false,
-});
-const tokenText = new TokenText({
-  type: TOKEN_TYPE.SEPARATOR,
-  value: SEPARATOR.SEMICOLON.value,
-  hidden: false,
-});
-const tokens = [tokenHeader, tokenHeaderWithList, token, tokenField, tokenList, tokenText];
-
-function testTokenType(token: TokenGlobal) {
-  switch (token.type) {
-    case "number":
-      console.log(token);
-      break;
-    case "bracket":
-      console.log(token);
-      break;
-    case "column":
-      console.log(token);
-      break;
-    case "comma":
-      console.log(token);
-      break;
-    case "condition":
-      console.log(token);
-      break;
-    case "expression":
-      console.log(token);
-      break;
-    case "field":
-      console.log(token);
-      break;
-    case "header":
-      console.log(token);
-      break;
-    case "header_with_list":
-      console.log(token);
-      break;
-    case "hook":
-      console.log(token);
-      break;
-    case "jumpLine":
-      console.log(token);
-      break;
-    case "list":
-      console.log(token.children);
-      break;
-    case "operator":
-      console.log(token);
-      break;
-    case "parenthese":
-      console.log(token);
-      break;
-    case "quote":
-      console.log(token);
-      break;
-    case "separator":
-      console.log(token);
-      break;
-    case "space":
-      console.log(token);
-      break;
-    case "text":
-      console.log(token);
-      break;
-  }
-}
-
-// console.log(token);
-// console.log(tokenField);
-// console.log(tokenList);
-console.log(tokenHeaderWithList.getTitle());
-console.log(tokens.reduce((acc, token) => `${acc}${token.toString()}`, ""));
+export { OPERATOR } from "./constants/operators.js";
+export { OUTPUT_FORMAT } from "./constants/output-format.js";
+export { SEPARATOR } from "./constants/separator.js";
+export { TOKEN_TITLE } from "./constants/token-title.js";
+export { TOKEN_TYPE } from "./constants/token-type.js";
+export type * from "./interfaces/token.js";
+export type * from "./interfaces/token-contructor.js";
+export type * from "./interfaces/token-global.js";
+export type * from "./interfaces/utils.js";
+export { ArrayToString, ERROR_MSG } from "./interfaces/utils.js";
